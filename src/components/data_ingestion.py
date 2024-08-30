02b7019dfc4cbfb4ee1e92fa6cccc6289002c57d
shreyas-w <shreyaswandkar@gmail.com>
2024-08-30 16:23:55 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+
 # creating  a data class
 # dataclass helps us to initialize the class components without creating __init__
 
@@ -64,3 +66,11 @@ if __name__=="__main__":
 
   data_transformation=DataTransformation()
   train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+  # print("train_arr=",train_arr)
+  # print("test_arr=",test_arr)
+
+  modeltrainer=ModelTrainer()
+  print(modeltrainer.initaite_model_trainer(train_arr,test_arr))
+
+
+
